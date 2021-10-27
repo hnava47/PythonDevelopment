b5833d7bbdd4e5527d344a080a86f00faa8ff9fe
Hector Nava <hector.r.nava44@gmail.com>
2021-10-26 21:01:34 -0600

    Added less complex solution

--- Scripts/SampleQ/TwoNumberSum/twoNumberSum.py
+++ Scripts/SampleQ/TwoNumberSum/twoNumberSum.py
@@ -1,7 +1,17 @@
+# O(n) time | O(1) space
 def twoNumberSum(array, targetSum):
-    for i in range(len(array)-1):
-		for k in range(i+1, len(array)):
-			if array[i] + array[k] == targetSum:
-				return [array[i], array[k]]
-	else:
-		return []
+    # Write your code here.
+	result = []
+	for num in array:
+		offset = targetSum - num
+		if offset in array:
+			result = [offset, num]
+	return result
+
+# def twoNumberSum(array, targetSum):
+#     for i in range(len(array)-1):
+# 		for k in range(i+1, len(array)):
+# 			if array[i] + array[k] == targetSum:
+# 				return [array[i], array[k]]
+# 	else:
+# 		return []
