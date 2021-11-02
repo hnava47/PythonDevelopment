0d7ec63459ae87d1230983bda940ce3f285010ec
Hector Nava <hector.r.nava44@gmail.com>
2021-11-02 15:10:34 -0600

    Init repo

---
+++ Scripts/SampleQ/InsertionSort/insertionSort.py
@@ -0,0 +1,8 @@
+# O(n^2) time | O(1) space
+def insertionSort(array):
+	for i in range(1, len(array)):
+		j = i
+		while j > 0 and array[j] < array[j - 1]:
+			array[j], array[j - 1] = array[j - 1], array[j]
+			j -= 1
+	return array
---
+++ Scripts/SampleQ/InsertionSort/test_insertionSort.py
@@ -0,0 +1,12 @@
+import insertionSort as ins
+import unittest as ut
+
+class TestInsertionSort(ut.TestCase):
+	def test_case_one(self):
+		array = [8, 5, 2, 9, 5, 6, 3]
+		result = ins.insertionSort(array)
+		expected = [2, 3, 5, 5, 6, 8, 9]
+		self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+	ut.main()
